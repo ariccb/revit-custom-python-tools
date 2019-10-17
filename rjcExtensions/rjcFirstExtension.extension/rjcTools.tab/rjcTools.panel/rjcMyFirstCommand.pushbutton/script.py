5900f13236a7de5dc24f0b2bf41d7b9f7346a5db
Aric Crosson Bouwers <acrossonbouwers@rjc.local>
2019-10-17 14:41:05 -0600

    Adding Tools via PyRevit
    Test command, to see if functionality is working

---
+++ rjcExtensions/rjcFirstExtension.extension/rjcTools.tab/rjcTools.panel/rjcMyFirstCommand.pushbutton/script.py
@@ -0,0 +1,37 @@
+"""Calculates total volume of all walls in the model."""
+
+#Set sys.path
+import sys
+sys.path.insert(0, 'C:\Program files (x86)\IronPython 2.7\Lib')
+
+# Python module
+import traceback
+
+# .Net module
+import System
+
+# Common Language Runtime module (clr)
+import clr
+clr.AddReference('RevitAPI')
+clr.AddReference('RevitUI')
+clr.AddReference('RevitServices')
+clr.AddReference('RevitNodes')
+clr.AddReference('ProtoGeometry')
+
+from Autodesk.Revit import DB
+
+doc = __revit__.ActiveUIDocument.Document
+
+# Creating collector instance and collecting all the walls from the model
+wall_collector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Walls).WhereElementIsNotElementType()
+
+# Iterate over wall and collect Volume data
+total_volume = 0.0
+
+for wall in wall_collector:
+    vol_param = wall.Parameter[DB.BuiltInParameter.HOST_VOLUME_COMPUTED]
+    if vol_param:
+        total_volume = total_volume + vol_param.AsDouble()
+
+# now that results are collected, print the total
+print("Total Volume is: {}".format(total_volume))
